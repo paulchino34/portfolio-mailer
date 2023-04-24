3442873e67079f53416468a3de3ba1c4f9c84a94
paulchino34 <paul-jara@hotmail.com>
2023-04-24 04:10:37 -0500

    Actualizacion de envio de correo
    Actualizacion de envio de correo

--- src/api/mail.handler.js
+++ src/api/mail.handler.js
@@ -1,3 +1,7 @@
+import nodemailer from 'nodemailer'
+import { google } from 'googleapis'
+const OAuth2 = google.auth.OAuth2
+
 const sendMail = (req, res) => {
     const { firstName, lastName, email, phone, message } = req.body
     const contentHTLM = `
@@ -8,8 +12,52 @@ const sendMail = (req, res) => {
             <li><p>Message: ${message}</p></li>
         </ul>
     `
-    console.log(contentHTLM)
-    res.send('received')
+
+    const oAuth2Client = new OAuth2(
+        process.env.MAIL_CLIENT_ID,
+        process.env.MAIL_CLIENT_SECRET,
+        process.env.MAIL_REDIRECT_URI
+    )
+
+    oAuth2Client.setCredentials({ refresh_token: process.env.MAIL_REFRESH_TOKEN })
+
+    const sendFromGmail = async () => {
+        try {
+            const accessToken = await oAuth2Client.getAccessToken()
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    type: 'OAuth2',
+                    user: process.env.MAIL_FROM,
+                    clientId: process.env.MAIL_CLIENT_ID,
+                    clientSecret: process.env.MAIL_CLIENT_SECRET,
+                    refreshToken: process.env.MAIL_REFRESH_TOKEN,
+                    accessToken
+                }
+            })
+
+            transporter.verify((error) => {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log("Ready to Send")
+                }
+            })
+
+            const mailOptions = {
+                from: process.env.MAIL_FROM,
+                to: 'paul-jara@hotmail.com',
+                subject: 'Aviso de interes para Prestación de servicios',
+                html: contentHTLM
+            }
+            const result = await transporter.sendMail(mailOptions)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    sendFromGmail()
+        .then(result => res.status(200).send('Message Sent'))
+        .catch(error => console.log(error))
 }
 
 export { sendMail }
